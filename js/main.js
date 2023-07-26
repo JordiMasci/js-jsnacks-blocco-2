f5613c975987c4ba9b0b9b86551be68cdc20728c
JordiMasci <133970995+JordiMasci@users.noreply.github.com>
2023-07-26 09:54:55 +0200

    fix: snack- pari

---
+++ js/main.js
@@ -0,0 +1,14 @@
+const userNumber = parseInt(prompt('inserisci un numero'))
+
+
+if(userName % 2 == 0) {
+    console.log('Numero: ' + userNumber)
+}
+else {
+    console.log('Numero: ' + userNumber + 1)
+}
+
+
+// Chiedi un numero
+// Se pari stampalo
+// altrimenti stampa il numero +1
